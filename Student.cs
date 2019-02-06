5525bccd54259f19aab9951879a5d7c228044ad0
sanjiusf <43866731+sanjiusf@users.noreply.github.com>
2019-02-05 21:30:22 -0500

    Add files via upload
    version 1

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP_Introduction
+{
+  class Student : Person
+  {
+    public string studentNumber;
+    public Person Advisor;
+
+    /// <summary>
+    /// Default constructor attempt 1
+    /// </summary>
+    public Student()
+    {
+      studentNumber = "U1234";
+      lastName = "Doe";
+      Advisor = new  Person();
+    }
+
+    /// <summary>
+    /// Default constructor that uses inheritance
+    /// </summary>
+    //public Student() : base()
+    //{
+    //  studentNumber = "U1234";
+    //}
+  }
+}
---
+++ UndergraduateStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Introduction
+{
+  class UndergraduateStudent : Student, BestPractices
+  {
+    public string Study()
+    {
+      return "Great party last night. All set for the exam!";
+    }
+
+    public string Rest()
+    {
+      return "Great friends, great movies, great parties. Well rested";
+    }
+  }
+}
